98ea914abaaf0bc75772e5c4ac9099e4d8b35eed
-Pawan Rajput <pawanrajput852710@gmail.com>
2026-06-21 22:35:52 +0530

    changes

--- components/site/BrandLogo.tsx
+++ components/site/BrandLogo.tsx
@@ -8,14 +8,7 @@ export function BrandLogo({ compact = false }: { compact?: boolean }) {
             className="h-8 sm:h-10 lg:h-12 w-auto"
           />
         </div>
-      {!compact && (
-        <div className="text-left">
-          <p className="text-base sm:text-xl lg:text-2xl font-bold leading-tight">
-            <span className="text-white lg:text-[#001f3f]">Risk</span>
-            <span className="text-[#ffcc00]">Busters</span>
-          </p>
-        </div>
-      )}
+      
     </div>
   );
 }
